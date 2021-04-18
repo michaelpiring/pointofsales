2b57cd95d35484e137eb9cf17090fc0d3ba130ed
Michael Piring <piring.michael@gmail.com>
2021-04-18 17:46:53 +0800

    update penjualan
    validasi checkout (udah dibayar/belum dibayar), update saldo hutang user

--- app/Http/Controllers/PenjualanController.php
+++ app/Http/Controllers/PenjualanController.php
@@ -68,163 +68,190 @@ class PenjualanController extends Controller
             $data_keranjang = Keranjang::where('id_keranjang', $data_checkout['id_keranjang'])->first();
             $data_user = User::where('id', $data_checkout['id_user'])->first();
 
-            //cek metode pembayaran
-            if($data['metode_pembayaran']=="cash"){
-                //create data penjualan
-                $data['id_user'] = $data_user['id'];
-                $data['tgl_penjualan'] = now();
-                $data['total_penjualan'] = $data_checkout['total_checkout'];
-                $data['status'] = 'sudah dibayar';
-
-                $create_penjualan = Penjualan::create($data);
-
-                if($create_penjualan){
-                    //create detail penjualan & kurangin stok barang di tb produk
-                    $data_detail_keranjangs = DetailKeranjang::where('id_keranjang', $data_keranjang['id_keranjang'])->get();
-
-                    foreach($data_detail_keranjangs as $data_detail_keranjang){
-                        DetailPenjualan::create([
-                            'id_penjualan'  => $create_penjualan['id_penjualan'],
-                            'id_toko'       => $create_penjualan['id_toko'],
-                            'id_user'       => $create_penjualan['id_user'],
-                            'id_pegawai'    => $create_penjualan['id_pegawai'],
-                            'id_produk'     => $data_detail_keranjang['id_produk'],
-                            'tgl_penjualan' => now(),
-                            'jumlah_produk' => $data_detail_keranjang['jumlah_produk'],
-                            'total_harga'   => $data_detail_keranjang['total_harga']
-                        ]);
-
-                        //update stok di tb produk
-                        $data_produk = Produk::where('id_produk', $data_detail_keranjang['id_produk'])->first();
-
-                        $kurangi_stok = $data_produk['stok'] - $data_detail_keranjang['jumlah_produk'];
-                        $update_stok = Produk::where('id_produk', $data_detail_keranjang['id_produk'])->update([
-                            'stok' => $kurangi_stok
-                        ]);
-                    }
-                    $update_status_checkout = Checkout::where('id_checkout', $data['id_checkout'])->update([
-                        'status' => 'sudah dibayar'
-                    ]);
-
-                    return response()->json([
-                        'success' => true,
-                        'message' => 'Transaksi Berhasil!',
-                        'data'    => $data 
-                    ], 201);
-                }
+            if($data_checkout['status']=="sudah dibayar"){
+                //transaksi gagal, checkout telah dibayar, periksa kembali id checkout
+                return response()->json([
+                    'success' => false,
+                    'message' => 'Transaksi tersebut telah selesai!',
+                ], 404);
             }
+            else{
+                //status checkout = belum dibayar, lanjutkan transaksi
 
-            if($data['metode_pembayaran']=="hutang"){
-                //create data penjualan & hutang
-                $data['id_user'] = $data_user['id'];
-                $data['tgl_penjualan'] = now();
-                $data['total_penjualan'] = $data_checkout['total_checkout'];
-                $data['status'] = 'belum dibayar';
+                //cek metode pembayaran
+                if($data['metode_pembayaran']=="cash"){
+                    //create data penjualan
+                    $data['id_user'] = $data_user['id'];
+                    $data['tgl_penjualan'] = now();
+                    $data['total_penjualan'] = $data_checkout['total_checkout'];
+                    $data['status'] = 'sudah dibayar';
 
-                $create_penjualan = Penjualan::create($data);
+                    $create_penjualan = Penjualan::create($data);
 
-                if($create_penjualan){
-                    //create data hutang
-                    $create_hutang = Hutang::create([
-                        'id_checkout'   => $data['id_checkout'],
-                        'id_user'       => $data_checkout['id_user'],
-                        'tgl_hutang'    => now(),
-                        'besar_hutang'  => $data_checkout['total_checkout'],
-                        'status'        => 'belum lunas'
-                    ]);
+                    if($create_penjualan){
+                        //create detail penjualan & kurangin stok barang di tb produk
+                        $data_detail_keranjangs = DetailKeranjang::where('id_keranjang', $data_keranjang['id_keranjang'])->get();
 
-                    //create detail penjualan
-                    $data_detail_keranjangs = DetailKeranjang::where('id_keranjang', $data_keranjang['id_keranjang'])->get();
+                        foreach($data_detail_keranjangs as $data_detail_keranjang){
+                            DetailPenjualan::create([
+                                'id_penjualan'  => $create_penjualan['id_penjualan'],
+                                'id_toko'       => $create_penjualan['id_toko'],
+                                'id_user'       => $create_penjualan['id_user'],
+                                'id_pegawai'    => $create_penjualan['id_pegawai'],
+                                'id_produk'     => $data_detail_keranjang['id_produk'],
+                                'tgl_penjualan' => now(),
+                                'jumlah_produk' => $data_detail_keranjang['jumlah_produk'],
+                                'total_harga'   => $data_detail_keranjang['total_harga']
+                            ]);
 
-                    foreach($data_detail_keranjangs as $data_detail_keranjang){
-                        DetailPenjualan::create([
-                            'id_penjualan'  => $create_penjualan['id_penjualan'],
-                            'id_toko'       => $create_penjualan['id_toko'],
-                            'id_user'       => $create_penjualan['id_user'],
-                            'id_pegawai'    => $create_penjualan['id_pegawai'],
-                            'id_produk'     => $data_detail_keranjang['id_produk'],
-                            'tgl_penjualan' => now(),
-                            'jumlah_produk' => $data_detail_keranjang['jumlah_produk'],
-                            'total_harga'   => $data_detail_keranjang['total_harga']
+                            //update stok di tb produk
+                            $data_produk = Produk::where('id_produk', $data_detail_keranjang['id_produk'])->first();
+
+                            $kurangi_stok = $data_produk['stok'] - $data_detail_keranjang['jumlah_produk'];
+                            $update_stok = Produk::where('id_produk', $data_detail_keranjang['id_produk'])->update([
+                                'stok' => $kurangi_stok
+                            ]);
+                        }
+                        $update_status_checkout = Checkout::where('id_checkout', $data['id_checkout'])->update([
+                            'status' => 'sudah dibayar'
+                        ]);
+
+                        return response()->json([
+                            'success' => true,
+                            'message' => 'Transaksi Berhasil!',
+                            'data'    => $data 
+                        ], 201);
+                    }
+                }
+
+                if($data['metode_pembayaran']=="hutang"){ 
+                    //create data penjualan & hutang
+                    $data['id_user'] = $data_user['id'];
+                    $data['tgl_penjualan'] = now();
+                    $data['total_penjualan'] = $data_checkout['total_checkout'];
+                    $data['status'] = 'belum dibayar';
+    
+                    $create_penjualan = Penjualan::create($data);
+    
+                    if($create_penjualan){
+                        //create data hutang
+                        $create_hutang = Hutang::create([
+                            'id_checkout'   => $data['id_checkout'],
+                            'id_user'       => $data_checkout['id_user'],
+                            'tgl_hutang'    => now(),
+                            'besar_hutang'  => $data_checkout['total_checkout'],
+                            'status'        => 'belum lunas'
+                        ]);
+    
+                        //create detail penjualan
+                        $data_detail_keranjangs = DetailKeranjang::where('id_keranjang', $data_keranjang['id_keranjang'])->get();
+    
+                        foreach($data_detail_keranjangs as $data_detail_keranjang){
+                            DetailPenjualan::create([
+                                'id_penjualan'  => $create_penjualan['id_penjualan'],
+                                'id_toko'       => $create_penjualan['id_toko'],
+                                'id_user'       => $create_penjualan['id_user'],
+                                'id_pegawai'    => $create_penjualan['id_pegawai'],
+                                'id_produk'     => $data_detail_keranjang['id_produk'],
+                                'tgl_penjualan' => now(),
+                                'jumlah_produk' => $data_detail_keranjang['jumlah_produk'],
+                                'total_harga'   => $data_detail_keranjang['total_harga']
+                            ]);
+                            
+                            //update stok di tb produk
+                            $data_produk = Produk::where('id_produk', $data_detail_keranjang['id_produk'])->first();
+    
+                            $kurangi_stok = $data_produk['stok'] - $data_detail_keranjang['jumlah_produk'];
+                            $update_stok = Produk::where('id_produk', $data_detail_keranjang['id_produk'])->update([
+                                'stok' => $kurangi_stok
+                            ]);    
+                        }
+                        //update saldo_hutang user
+                        $saldo_hutang = $data_user['saldo_hutang']+$data_checkout['total_checkout'];
+                        $update_saldo_hutang = User::where('id', $data_checkout['id_user'])->update([
+                            'saldo_hutang' => $saldo_hutang
+                        ]);
+                        //update status checkout disini
+                        $update_status_checkout = Checkout::where('id_checkout', $data['id_checkout'])->update([
+                            'status' => 'sudah dibayar'
+                        ]);
+
+                        return response()->json([
+                            'success' => true,
+                            'message' => 'Transaksi Hutang Berhasil!',
+                            'data'    => $create_hutang 
+                        ], 201);
+                    }
+                }
+
+                if($data['metode_pembayaran']=="split"){
+                    //create data penjualan & hutang (split payment)
+                    //ganti total penjualan di tb penjualan
+                    //ganti besar hutang di tb hutang
+                    $data['id_user'] = $data_user['id'];
+                    $data['tgl_penjualan'] = now();
+                    $data['total_penjualan'] = $data['jumlah_bayar'];
+                    $data['status'] = 'belum dibayar'; //revisi status tb penjualan jadi->(lunas/hutang/split)
+    
+                    $create_penjualan = Penjualan::create($data);
+    
+                    if($create_penjualan){
+                        //create data hutang
+    
+                        //hitung sisa hutang
+                        $sisa_hutang = $data_checkout['total_checkout']-$data['jumlah_bayar'];
+    
+                        $create_hutang = Hutang::create([
+                            'id_checkout'   => $data['id_checkout'],
+                            'id_user'       => $data_checkout['id_user'],
+                            'tgl_hutang'    => now(),
+                            'besar_hutang'  => $sisa_hutang,
+                            'status'        => 'belum lunas'
+                        ]);
+    
+                        //create detail penjualan
+                        $data_detail_keranjangs = DetailKeranjang::where('id_keranjang', $data_keranjang['id_keranjang'])->get();
+    
+                        foreach($data_detail_keranjangs as $data_detail_keranjang){
+                            DetailPenjualan::create([
+                                'id_penjualan'  => $create_penjualan['id_penjualan'],
+                                'id_toko'       => $create_penjualan['id_toko'],
+                                'id_user'       => $create_penjualan['id_user'],
+                                'id_pegawai'    => $create_penjualan['id_pegawai'],
+                                'id_produk'     => $data_detail_keranjang['id_produk'],
+                                'tgl_penjualan' => now(),
+                                'jumlah_produk' => $data_detail_keranjang['jumlah_produk'],
+                                'total_harga'   => $data_detail_keranjang['total_harga']
+                            ]);
+                            
+                            //update stok di tb produk
+                            $data_produk = Produk::where('id_produk', $data_detail_keranjang['id_produk'])->first();
+    
+                            $kurangi_stok = $data_produk['stok'] - $data_detail_keranjang['jumlah_produk'];
+                            $update_stok = Produk::where('id_produk', $data_detail_keranjang['id_produk'])->update([
+                                'stok' => $kurangi_stok
+                            ]);
+    
+                        }
+                        //update saldo_hutang user
+                        $saldo_hutang = $data_user['saldo_hutang']+$sisa_hutang;
+                        $update_saldo_hutang = User::where('id', $data_checkout['id_user'])->update([
+                            'saldo_hutang' => $saldo_hutang
+                        ]);
+
+                        //update status checkout disini
+                        $update_status_checkout = Checkout::where('id_checkout', $data['id_checkout'])->update([
+                            'status' => 'sudah dibayar'
                         ]);
                         
-                        //update stok di tb produk
-                        $data_produk = Produk::where('id_produk', $data_detail_keranjang['id_produk'])->first();
-
-                        $kurangi_stok = $data_produk['stok'] - $data_detail_keranjang['jumlah_produk'];
-                        $update_stok = Produk::where('id_produk', $data_detail_keranjang['id_produk'])->update([
-                            'stok' => $kurangi_stok
-                        ]);
-
+                        return response()->json([
+                            'success' => true,
+                            'message' => 'Transaksi Split Berhasil!',
+                            'data'    => $create_hutang 
+                        ], 201);
                     }
-                    //update status checkout disini
-
-                    return response()->json([
-                        'success' => true,
-                        'message' => 'Transaksi Hutang Berhasil!',
-                        'data'    => $create_hutang 
-                    ], 201);
                 }
-            }
-
-            if($data['metode_pembayaran']=="split"){
-                //create data penjualan & hutang (split payment)
-                //ganti total penjualan di tb penjualan
-                //ganti besar hutang di tb hutang
-                $data['id_user'] = $data_user['id'];
-                $data['tgl_penjualan'] = now();
-                $data['total_penjualan'] = $data['jumlah_bayar'];
-                $data['status'] = 'belum dibayar'; //revisi status tb penjualan jadi->(lunas/hutang/split)
-
-                $create_penjualan = Penjualan::create($data);
-
-                if($create_penjualan){
-                    //create data hutang
-
-                    //hitung sisa hutang
-                    $sisa_hutang = $data_checkout['total_checkout']-$data['jumlah_bayar'];
-
-                    $create_hutang = Hutang::create([
-                        'id_checkout'   => $data['id_checkout'],
-                        'id_user'       => $data_checkout['id_user'],
-                        'tgl_hutang'    => now(),
-                        'besar_hutang'  => $sisa_hutang,
-                        'status'        => 'belum lunas'
-                    ]);
-
-                    //create detail penjualan
-                    $data_detail_keranjangs = DetailKeranjang::where('id_keranjang', $data_keranjang['id_keranjang'])->get();
-
-                    foreach($data_detail_keranjangs as $data_detail_keranjang){
-                        DetailPenjualan::create([
-                            'id_penjualan'  => $create_penjualan['id_penjualan'],
-                            'id_toko'       => $create_penjualan['id_toko'],
-                            'id_user'       => $create_penjualan['id_user'],
-                            'id_pegawai'    => $create_penjualan['id_pegawai'],
-                            'id_produk'     => $data_detail_keranjang['id_produk'],
-                            'tgl_penjualan' => now(),
-                            'jumlah_produk' => $data_detail_keranjang['jumlah_produk'],
-                            'total_harga'   => $data_detail_keranjang['total_harga']
-                        ]);
-                        
-                        //update stok di tb produk
-                        $data_produk = Produk::where('id_produk', $data_detail_keranjang['id_produk'])->first();
-
-                        $kurangi_stok = $data_produk['stok'] - $data_detail_keranjang['jumlah_produk'];
-                        $update_stok = Produk::where('id_produk', $data_detail_keranjang['id_produk'])->update([
-                            'stok' => $kurangi_stok
-                        ]);
-
-                    }
-                    //update status checkout disini
-
-                    return response()->json([
-                        'success' => true,
-                        'message' => 'Transaksi Split Berhasil!',
-                        'data'    => $create_hutang 
-                    ], 201);
-                }
-            }
+            } 
         }
         else{
             return response()->json([
