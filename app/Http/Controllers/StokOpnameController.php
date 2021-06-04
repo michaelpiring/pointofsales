c6e4b9f97374ec754903b27a177833738a5077b2
Aditya Ersa <haganeaditya@gmail.com>
2021-06-04 14:19:45 +0800

    Update StokOpnameController.php

--- app/Http/Controllers/StokOpnameController.php
+++ app/Http/Controllers/StokOpnameController.php
@@ -23,7 +23,18 @@ class StokOpnameController extends Controller
      */
     public function index()
     {
-        //
+        $data = StokOpname::where('status', '!=', 'unapproved')->where('status','!=','canceled')->where('status','!=','on progress')->get();
+        if($data){
+            return response()->json([
+                'success' => true,
+                'data' => $data
+            ],201);
+        }
+        else{
+            return response()->json([
+                'success' => false,
+            ],401);
+        }
     }
 
     /**
@@ -32,6 +43,7 @@ class StokOpnameController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
+
     public function store(Request $request)
     {
         $validate = $request->validate([
@@ -46,9 +58,10 @@ class StokOpnameController extends Controller
                 'tgl_stok_opname' => now(),
                 'status' => 'on progress'
             ]);
-            $data['stok_opname_terakhir'] = $stok_opname['tgl_stok_opname'];
             if(!$stok_opname){
                 $data['stok_opname_terakhir'] = date('2018-01-01');
+            }else{
+                $data['stok_opname_terakhir'] = $stok_opname['tgl_stok_opname'];
             }
             return response()->json([
                 'success' => true,
@@ -65,7 +78,7 @@ class StokOpnameController extends Controller
     // ini buat nyimpen data stokopname sementar
     // request berupa id stok opname, id produk, stok fisik dan stok opname terakhir
     // 
-    public function stokOpnameProdukSementara(Request $request){
+    public function getDetailProductForStockOpname(Request $request){
         $validate = $request->validate([
             'id_toko' => 'required',
             'id_stok_opname' => 'required',
@@ -74,24 +87,30 @@ class StokOpnameController extends Controller
             'stok_opname_terakhir' => 'required',
             'keterangan' => 'required'
         ]);
-        if($validate){
-            $stok_opname = StokOpname::where(['id_stok_opname', $validate['id_stok_opname']])->first();
+        if($validate){            
             $produk = Produk::where([['id_produk', $validate['id_produk']]])->first();
-            $penjualan = DetailPenjualan::where([['id_toko', $validate['id_toko']],
-                                                 ['id_produk',$validate['id_produk']]])->whereBetween('tgl_penjualan',[$$validate['stok_opname_terakhir'], now()])->sum('jumlah_produk');
-            $pembelian = DetailPembelian::where([['id_toko', $validate['id_toko']],
-                                                 ['id_produk', $validate['id_produk']]])->whereBetween('tgl_pembelian',[$$validate['stok_opname_terakhir'], now()])->sum('jumlah_produk'); 
+            $penjualan = DetailPenjualan::where('id_toko',$validate['id_toko'])
+                                        ->where('id_produk',$validate['id_produk'])->whereBetween('tgl_penjualan', [$validate['stok_opname_terakhir'], now()])->sum('jumlah_produk');
+            $pembelian = Pembelian::where([['id_toko', $validate['id_toko']],
+                                            ['id_produk', $validate['id_produk']]])->whereBetween('tgl_pembelian',[$validate['stok_opname_terakhir'], now()])->sum('jumlah_barang'); 
         
-            if($stok_opname['status'] == 'on progress'){
-                DetailStokOpnameSementara::create([
+            if($produk){
+                $data = ([
                     'id_stok_opname' => $request->id_stok_opname,
                     'id_produk' => $request->id_produk,
-                    'stok_masuk' => $pembelian,
-                    'stok_keluar' => $penjualan,
+                    'nama_produk' => $produk['nama_produk'],
+                    'stok_masuk' => $penjualan,
+                    'stok_keluar' => $pembelian,
                     'stok_sistem' => $produk['stok'],
                     'stok_fisik' => $request->stok_fisik,
                     'selisih' => $request->stok_fisik - $produk['stok'],
                     'keterangan' => $request->keterangan
+                ]);
+                return response()->json([
+                    'success' => true,
+                    'data' => $data,
+                    'penjualan' => $penjualan,
+                    'pembelian' => $pembelian
                 ]);
             }
             else{
@@ -101,33 +120,13 @@ class StokOpnameController extends Controller
             }                                     
         }
     }
-
-    public function showStokOpnameSementara(Request $request){
-        $validate = $request->validate([
-            'id_toko' => 'required',
-            'id_stok_opname' => 'required',
-        ]);
-        if($validate){
-            $data = DetailStokOpnameSementara::where('id_stok_opname', $validate['id_stok_opname']);
-            return response()->json([
-                'success' => true,
-                'data' => $data
-            ]);
-        }else{
-            return response()->json([
-                'success' => false
-            ]);
-        }
-    }
-
     public function storeStokOpname(Request $request){
         $validate = $request->validate([
             'id_toko' => 'required',
             'id_stok_opname' => 'required'
         ]);
-
+        $data = $request->datas;
         if($validate){
-            $data = DetailStokOpnameSementara::where('id_stok_opname', $validate['id_stok_opname']);
             $stok_opname = StokOpname::where('id_stok_opname', $validate['id_stok_opname']);
             if($data){
                 foreach($data as $data){
@@ -141,14 +140,13 @@ class StokOpnameController extends Controller
                         'selisih' => $data['selisih'],
                         'keterangan' => $data['keterangan']
                     ]);
-                    DetailStokOpnameSementara::where('id_stok_opname', $validate['id_stok_opname'])->delete();
-                    $stok_opname->update([
-                        'status' => 'pending validation'
-                    ]);
-                    return response()->json([
-                        'success' => true
-                    ],201);
                 }
+                $stok_opname->update([
+                    'status' => 'pending validation'
+                ]);
+                return response()->json([
+                    'success' => true,
+                ],201);
 
             }
             else{
@@ -159,34 +157,57 @@ class StokOpnameController extends Controller
         }
         else{
             return response()->json([
-                'success' => false
+                'success' => false,
+                'data' => $data
             ],400);
         }
     }
     public function approveStokOpname($id){
+        $cek_user = auth()->user();
+
         $data = StokOpname::where('id_stok_opname', $id)->first();
         if($data){
-            $data->update(['status' => 'approved']);
-            return response()->json([
-                'success' => true
-            ],201);
+            if($cek_user['id_jabatan'] == '2'){
+                $data->update(['status' => 'approved']);
+                return response()->json([
+                    'success' => true
+                ],201);
+            }else{
+                return response()->json([
+                    'success' => false,
+                    'message' => 'Validasi Pembelian hanya dapat dilakukan Kepala Gudang!',
+                ], 409);
+            }
         }else{
             return response()->json([
-                'success' => false
+                'success' => false,
+                'id' => $id,
+                'data' => $data
             ],401);
         }
     }
 
     public function unapproveStokOpname($id){
+        $cek_user = auth()->user();
+
         $data = StokOpname::where('id_stok_opname', $id)->first();
         if($data){
-            $data->update(['status' => 'unapproved']);
-            return response()->json([
-                'success' => true
-            ],201);
+            if($cek_user['id_jabatan'] == '2'){
+                $data->update(['status' => 'unapproved']);
+                return response()->json([
+                    'success' => true
+                ],201);
+            }else{
+                return response()->json([
+                    'success' => false,
+                    'message' => 'Validasi Pembelian hanya dapat dilakukan Kepala Gudang!',
+                ], 409);
+            }
         }else{
             return response()->json([
-                'success' => false
+                'success' => false,
+                'id' => $id,
+                'data' => $data
             ],401);
         }
     }
@@ -210,7 +231,28 @@ class StokOpnameController extends Controller
      */
     public function update(Request $request, $id)
     {
-        //
+        $cek_user = auth()->user();
+
+        $data = StokOpname::where('id_stok_opname', $id)->first();
+        if($data){
+            if($cek_user['id_jabatan'] == '2'){
+                $data->update(['status' => 'approved']);
+                return response()->json([
+                    'success' => true
+                ],201);
+            }else{
+                return response()->json([
+                    'success' => false,
+                    'message' => 'Validasi Pembelian hanya dapat dilakukan Kepala Gudang!',
+                ], 409);
+            }
+        }else{
+            return response()->json([
+                'success' => false,
+                'id' => $id,
+                'data' => $data
+            ],401);
+        }
     }
 
     /**
@@ -219,25 +261,29 @@ class StokOpnameController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function destroy(Request $request)
+    public function destroy($id)
     {
-        $validate = $request->validate([
-            'id_toko' => 'required',
-            'id_stok_opname' => 'required'
-        ]);
-        if($validate){
-            StokOpname::where([['id_stok_opname', $validate['id_stok_opname']],
-                               ['id_toko', $validate['id_toko']]])->update([
-                                   'status' => 'canceled'
-                               ]);
+        $cek_user = auth()->user();
+
+        $data = StokOpname::where('id_stok_opname', $id)->first();
+        if($data){
+            if($cek_user['id_jabatan'] == '2'){
+                $data->update(['status' => 'approved']);
+                return response()->json([
+                    'success' => true
+                ],201);
+            }else{
+                return response()->json([
+                    'success' => false,
+                    'message' => 'Validasi Pembelian hanya dapat dilakukan Kepala Gudang!',
+                ], 409);
+            }
+        }else{
             return response()->json([
-                'success' => true
-            ]);
-        }
-        else{
-            return response()->json([
-                'success' => false
-            ]);
+                'success' => false,
+                'id' => $id,
+                'data' => $data
+            ],401);
         }
     }
 }
